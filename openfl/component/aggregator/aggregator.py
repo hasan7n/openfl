71d6917d75eb4e577743449d3522538c5a23f6d8
Edwards, Brandon <brandon.edwards@intel.com>
2024-10-25 10:39:52 -0700

    more debug prints

--- openfl/component/aggregator/aggregator.py
+++ openfl/component/aggregator/aggregator.py
@@ -700,6 +700,10 @@ class Aggregator:
         # (if more data is added)
         self.collaborator_task_weight[task_key] = data_size
 
+        print(f"\nBrandon DEBUG - setting col weight")
+        print(f"{KEY:{task_key}}")
+        print(f"Value:{data_size}\n")
+
         # initialize the list of tensors that go with this task
         # Setting these incrementally is leading to missing values
         task_results = []
@@ -989,6 +993,9 @@ class Aggregator:
             k: v / weight_total
             for k, v in collaborator_weights_unnormalized.items()
         }
+
+        print(f"\n Brandon DEBUG - Aggregating metrics for task:{task_name}")
+        print(f"col_weights:{collaborator_weight_dict}\n")
 
         # The validation task should have just a couple tensors (i.e.
         # metrics) associated with it. Because each collaborator should
