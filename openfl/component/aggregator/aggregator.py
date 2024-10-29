7047fd81437ede725537ead23bcdcbe6c3264246
Edwards, Brandon <brandon.edwards@intel.com>
2024-10-29 12:29:53 -0700

    correct typo

--- openfl/component/aggregator/aggregator.py
+++ openfl/component/aggregator/aggregator.py
@@ -208,8 +208,8 @@ class Aggregator:
             self.logger.info(f"No such keys {task_name} and {arg_name} in:\n{self.dynamictaskargs}")
             raise ValueError(f"FL plan is not configured to set dynamic task arg:{arg_name} for task:{task_name}")
         elif 'min' in self.dynamictaskargs[task_name][arg_name] and value < self.dynamictaskargs[task_name][arg_name]['min']:
-            self.logger.info(f"Value:{value} for dynamic task arg:{arg_name} for task:{task_name} is less than the mininum configured in the plan:{self.dynamictaskargs[task_name][arg_name]['min']}")
-            raise ValueError(f"Value:{value} for dynamic task arg:{arg_name} for task:{task_name} is less than the mininum configured in the plan:{self.dynamictaskargs[task_name][arg_name]['min']}")
+            self.logger.info(f"Value:{value} for dynamic task arg:{arg_name} for task:{task_name} is less than the minimum configured in the plan:{self.dynamictaskargs[task_name][arg_name]['min']}")
+            raise ValueError(f"Value:{value} for dynamic task arg:{arg_name} for task:{task_name} is less than the minimum configured in the plan:{self.dynamictaskargs[task_name][arg_name]['min']}")
         elif 'max' in self.dynamictaskargs[task_name][arg_name]  and value > self.dynamictaskargs[task_name][arg_name]['max']:
             self.logger.info(f"Value:{value} for dynamic task arg:{arg_name} for task:{task_name} is greater than the maximum configured in the plan:{self.dynamictaskargs[task_name][arg_name]['max']}")
             raise ValueError(f"Value:{value} for dynamic task arg:{arg_name} for task:{task_name} is greater than the maximum configured in the plan:{self.dynamictaskargs[task_name][arg_name]['max']}")
