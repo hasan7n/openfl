fad968d0f96a9618177dde1b4d817800e10bc5ea
Edwards, Brandon <brandon.edwards@intel.com>
2024-11-19 13:00:06 -0800

    restoring idempotency for get_aggregated_tensor

--- openfl/databases/tensor_db.py
+++ openfl/databases/tensor_db.py
@@ -172,7 +172,7 @@ class TensorDB:
                                 & (self.tensor_db['report'] == report)
                                 & (self.tensor_db['tags'] == tags)]['nparray']
         if len(raw_df) > 0:
-            return np.array(raw_df.iloc[0]) # , {}
+            return np.array(raw_df.iloc[0]), {}
 
         for col in collaborator_names:
             new_tags = change_tags(tags, add_field=col)
