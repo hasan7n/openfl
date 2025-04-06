a7cf0363c26d92c94cff65fcdc0fc1137cbeb9da
hasan7n <hasankassim7@hotmail.com>
2025-04-06 14:02:47 +0000

    configure keepalive in grpc client and server

--- openfl/transport/grpc/grpc_channel_options.py
+++ openfl/transport/grpc/grpc_channel_options.py
@@ -1,11 +1,17 @@
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-max_metadata_size = 32 * 2 ** 20
-max_message_length = 2 ** 30
+max_metadata_size = 32 * 2**20
+max_message_length = 2**30
 
 channel_options = [
-    ('grpc.max_metadata_size', max_metadata_size),
-    ('grpc.max_send_message_length', max_message_length),
-    ('grpc.max_receive_message_length', max_message_length)
+    ("grpc.max_metadata_size", max_metadata_size),
+    ("grpc.max_send_message_length", max_message_length),
+    ("grpc.max_receive_message_length", max_message_length),
+    ("grpc.keepalive_time_ms", 20000),
+    ("grpc.keepalive_timeout_ms", 60000),
+    ("grpc.http2.max_pings_without_data", 0),
+    ("grpc.keepalive_permit_without_calls", 1),
+    ("grpc.max_connection_idle_ms", 20000),
+    ("grpc.http2.max_ping_strikes", 0),
 ]
